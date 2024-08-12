9089ebedddf48ea3600d79a9ea60edc5ada2c48a
jwaynick99 <jwaynick99@gmail.com>
2024-08-11 22:17:34 -0500

    GA5 Validate Function, Dependencies installed

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,6 @@
 // Write your helper functions here!
 
-require('cross-fetch/polyfill');
+require("cross-fetch/polyfill");
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
@@ -15,30 +15,32 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="">
     */
- }
- 
- function validateInput(testInput) {
-    
- }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
- 
- module.exports.addDestinationInfo = addDestinationInfo;
- module.exports.validateInput = validateInput;
- module.exports.formSubmission = formSubmission;
- module.exports.pickPlanet = pickPlanet; 
- module.exports.myFetch = myFetch;
+}
+
+function validateInput(testInput) {
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(Number(testInput))) {
+        return "Not a Number";
+    } else {
+        return "Is a Number";
+    }
+}
+
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {}
+
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch().then(function (response) {});
+
+    return planetsReturned;
+}
+
+function pickPlanet(planets) {}
+
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
